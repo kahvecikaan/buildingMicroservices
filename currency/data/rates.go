dc169e9c0db336da7c7b47fd64932cc19152e781
Kaan Kahveci <kaankahveci29@outlook.com>
2024-10-12 18:09:43 +0300

    Implement rate monitoring and adjust the rate changes accordingly

--- currency/data/rates.go
+++ currency/data/rates.go
@@ -4,8 +4,10 @@ import (
 	"encoding/xml"
 	"fmt"
 	"github.com/hashicorp/go-hclog"
+	"math/rand"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 type ExchangeRates struct {
@@ -21,18 +23,60 @@ func NewRates(logger hclog.Logger) (*ExchangeRates, error) {
 	return er, err
 }
 
-func (e *ExchangeRates) GetRates(base, dest string) (float64, error) {
+func (e *ExchangeRates) GetRate(base, dest string) (float64, error) {
 	br, ok := e.rates[base]
 	if !ok {
-		return 0, fmt.Errorf("Rate not found for currency %s", base)
+		return 0, fmt.Errorf("rate not found for currency %s", base)
 	}
 
 	dr, ok := e.rates[dest]
 	if !ok {
-		return 0, fmt.Errorf("Rate not found for currency %s", dest)
+		return 0, fmt.Errorf("rate not found for currency %s", dest)
 	}
 
 	return dr / br, nil
+}
+
+// MonitorRates checks the rates in the ECB API every interval and sends a message to the
+// returned channel when there are changes
+//
+// Note: the ECB API only returns data once a day, this function only simulates the changes
+// in rates for demonstration purposes
+func (e *ExchangeRates) MonitorRates(interval time.Duration) chan struct{} {
+	ret := make(chan struct{})
+
+	go func() {
+		ticker := time.NewTicker(interval)
+		for {
+			select {
+			case <-ticker.C:
+				// just add a random difference to the rate and return it
+				// this simulates the fluctuations in currency rates
+				for k, v := range e.rates {
+					// change can be 10% of original value
+					change := rand.Float64() / 10
+					// is this a pos. or neg. change
+					direction := rand.Intn(1)
+
+					if direction == 0 {
+						// new value will be min 90% of old
+						change = 1 - change
+					} else {
+						// new value will be 110% of old
+						change = 1 + change
+					}
+
+					// modify the rate
+					e.rates[k] = v * change
+				}
+
+				// notify updates, this will block unless there is a listener on the other end
+				ret <- struct{}{}
+			}
+		}
+	}()
+
+	return ret
 }
 
 func (e *ExchangeRates) getRates() error {

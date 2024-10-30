dc095d6b1c706d0967b0d1054327cb10902cf7d5
Kaan Kahveci <kaankahveci29@outlook.com>
2024-10-30 20:25:03 +0300

    Implement service layer for interacting with microservices in new architecture

---
+++ product-api/internal/service/currency.go
@@ -0,0 +1,241 @@
+package service
+
+import (
+	"context"
+	"github.com/hashicorp/go-hclog"
+	"github.com/kahvecikaan/buildingMicroservices/currency/protos"
+	"github.com/kahvecikaan/buildingMicroservices/product-api/internal/events"
+	"google.golang.org/grpc/status"
+	"sync"
+	"time"
+)
+
+type CurrencyService interface {
+	GetRate(ctx context.Context, base, destination string) (float64, error)
+	SubscribeToRates(ctx context.Context, currencies []string) error
+	ListAvailableCurrencies(ctx context.Context) ([]string, error)
+	Close() error
+}
+
+type currencyService struct {
+	log           hclog.Logger
+	client        protos.CurrencyClient
+	rates         map[string]float64
+	ratesMutex    sync.RWMutex
+	stream        protos.Currency_SubscribeRatesClient
+	subscriptions map[string]struct{}
+	subMutex      sync.RWMutex
+	closeCh       chan struct{}
+	eventBus      *events.EventBus[any]
+}
+
+func NewCurrencyService(
+	logger hclog.Logger,
+	client protos.CurrencyClient,
+	eventBus *events.EventBus[any]) CurrencyService {
+	svc := &currencyService{
+		log:           logger,
+		client:        client,
+		rates:         make(map[string]float64),
+		subscriptions: make(map[string]struct{}),
+		closeCh:       make(chan struct{}),
+		eventBus:      eventBus,
+	}
+
+	// Initialize the stream
+	if err := svc.initializeStream(context.Background()); err != nil {
+		logger.Error("Failed to initialize the stream", "error", err)
+	}
+
+	// Start handling rate updates and heartbeat
+	go svc.handleRateUpdates()
+	go svc.handleHeartbeat()
+
+	return svc
+}
+
+func (s *currencyService) initializeStream(ctx context.Context) error {
+	stream, err := s.client.SubscribeRates(ctx)
+	if err != nil {
+		s.log.Error("Error establishing subscription stream", "error", err)
+		return err
+	}
+	s.stream = stream
+	return nil
+}
+
+func (s *currencyService) handleHeartbeat() {
+	ticker := time.NewTimer(1 * time.Minute)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			if s.stream == nil {
+				s.log.Error("Bi-directional stream is nil, attempting to reinitialize")
+				if err := s.initializeStream(context.Background()); err != nil {
+					continue
+				}
+			}
+			heartbeat := &protos.RateRequest{
+				Base:        protos.Currencies_UNKNOWN,
+				Destination: protos.Currencies_UNKNOWN,
+			}
+
+			if err := s.stream.Send(heartbeat); err != nil {
+				s.log.Error("Failed to send heartbeat", "error", err)
+				// Attempt to reinitialize the stream
+				_ = s.initializeStream(context.Background())
+			} else {
+				s.log.Debug("Heartbeat send successfully")
+			}
+		case <-s.closeCh:
+			return
+		}
+	}
+}
+
+func (s *currencyService) handleRateUpdates() {
+	for {
+		select {
+		case <-s.closeCh:
+			return
+		default:
+			if s.stream == nil {
+				s.log.Error("Bidirectional stream is nil, waiting before retry")
+				time.Sleep(5 * time.Second)
+				continue
+			}
+
+			response, err := s.stream.Recv()
+			if err != nil {
+				s.log.Error("Error receiving rate updates", "error", err)
+				// Attempting to reinitialize the stream
+				_ = s.initializeStream(context.Background())
+				time.Sleep(5 * time.Second)
+				continue
+			}
+
+			switch msg := response.Message.(type) {
+			case *protos.StreamingRateResponse_RateResponse:
+				currency := msg.RateResponse.GetDestination().String()
+				newRate := msg.RateResponse.GetRate()
+
+				s.ratesMutex.Lock()
+				oldRate, exists := s.rates[currency]
+				s.rates[currency] = newRate
+				s.ratesMutex.Unlock()
+
+				s.log.Debug(
+					"Updated rate",
+					"destination", msg.RateResponse.GetDestination().String(),
+					"rate", msg.RateResponse.GetRate())
+
+				// Only publish the event if rate actually changed
+				if !exists || oldRate != newRate {
+					// Publish rate changed event
+					s.eventBus.Publish(events.RateChanged{
+						Currency: currency,
+						NewRate:  newRate,
+					})
+				}
+			case *protos.StreamingRateResponse_Error:
+				s.log.Error("Received error from server", "error", msg.Error.GetMessage())
+			}
+		}
+	}
+}
+
+func (s *currencyService) GetRate(ctx context.Context, base, destination string) (float64, error) {
+	s.log.Debug("Getting exchange rate", "base", base, "destination", destination)
+
+	// check if rate is already available
+	s.ratesMutex.RLock()
+	rate, ok := s.rates[destination]
+	s.ratesMutex.RUnlock()
+	if ok {
+		return rate, nil
+	}
+
+	// Request new rate via gRPC call
+	rateRequest := &protos.RateRequest{
+		Base:        protos.Currencies(protos.Currencies_value[base]),
+		Destination: protos.Currencies(protos.Currencies_value[destination]),
+	}
+
+	resp, err := s.client.GetRate(ctx, rateRequest)
+	if err != nil {
+		grpcErr, _ := status.FromError(err)
+		s.log.Error(
+			"Error getting exchange rate",
+			"base", base,
+			"destination", destination,
+			"error", grpcErr.Message())
+		return 0, err
+	}
+
+	// Store the rate
+	s.ratesMutex.Lock()
+	s.rates[destination] = resp.Rate
+	s.ratesMutex.Unlock()
+
+	// Subscribe to rates for this pair
+	if err := s.SubscribeToRates(ctx, []string{destination}); err != nil {
+		s.log.Error("Failed to subscribe to rate updates", "error", err)
+	}
+	return resp.Rate, nil
+}
+
+func (s *currencyService) SubscribeToRates(ctx context.Context, currencies []string) error {
+	s.log.Debug("Subscribing to currency rate updates", "currencies", currencies)
+
+	if s.stream == nil {
+		if err := s.initializeStream(ctx); err != nil {
+			return err
+		}
+	}
+
+	s.subMutex.Lock()
+	for _, currency := range currencies {
+		// Check if already subscribed
+		if _, exists := s.subscriptions[currency]; exists {
+			continue
+		}
+		s.subscriptions[currency] = struct{}{}
+
+		rateRequest := &protos.RateRequest{
+			Base:        protos.Currencies(protos.Currencies_value["EUR"]),
+			Destination: protos.Currencies(protos.Currencies_value[currency]),
+		}
+
+		if err := s.stream.Send(rateRequest); err != nil {
+			s.log.Error("Error sending rate subscription request", "currency", currency, "error", err)
+			s.subMutex.Unlock()
+			return err
+		}
+	}
+	s.subMutex.Unlock()
+
+	return nil
+}
+
+func (s *currencyService) ListAvailableCurrencies(ctx context.Context) ([]string, error) {
+	s.log.Debug("Listing available currencies")
+
+	resp, err := s.client.ListCurrencies(ctx, &protos.Empty{})
+	if err != nil {
+		grpcErr, _ := status.FromError(err)
+		s.log.Error("Error listing currencies", "error", grpcErr.Message())
+		return nil, err
+	}
+
+	return resp.Currencies, nil
+}
+
+func (s *currencyService) Close() error {
+	close(s.closeCh)
+	if s.stream != nil {
+		return s.stream.CloseSend()
+	}
+	return nil
+}
---
+++ product-api/internal/service/product.go
@@ -0,0 +1,197 @@
+package service
+
+import (
+	"context"
+	"github.com/hashicorp/go-hclog"
+	"github.com/kahvecikaan/buildingMicroservices/product-api/internal/domain"
+	"github.com/kahvecikaan/buildingMicroservices/product-api/internal/events"
+	"github.com/kahvecikaan/buildingMicroservices/product-api/internal/repository"
+)
+
+type ProductService interface {
+	GetProducts(ctx context.Context, currency string) (Products, error)
+	GetProductByID(ctx context.Context, id int, currency string) (*domain.Product, error)
+	UpdateProduct(ctx context.Context, product *domain.Product) error
+	AddProduct(ctx context.Context, product *domain.Product) error
+	DeleteProduct(ctx context.Context, id int) error
+	ListCurrencies(ctx context.Context) ([]string, error)
+	Close() error
+}
+
+type productService struct {
+	repo            repository.ProductRepository
+	currencyService CurrencyService
+	eventBus        *events.EventBus[any]
+	logger          hclog.Logger
+	rateSubscriber  events.Subscriber[any]
+}
+
+type Products []*domain.Product
+
+func NewProductService(
+	repo repository.ProductRepository,
+	currencyService CurrencyService,
+	eventBus *events.EventBus[any],
+	logger hclog.Logger) ProductService {
+	ps := &productService{
+		repo:            repo,
+		currencyService: currencyService,
+		eventBus:        eventBus,
+		logger:          logger,
+	}
+
+	// Subscribe to events
+	ps.rateSubscriber = eventBus.Subscribe()
+
+	// Start handling rate change events
+	go ps.handleRateChanges()
+
+	return ps
+}
+
+func (s *productService) handleRateChanges() {
+	for event := range s.rateSubscriber {
+		if rateEvent, ok := event.(events.RateChanged); ok {
+			s.logger.Debug("Received rate changed event",
+				"currency", rateEvent.Currency,
+				"new_rate", rateEvent.NewRate)
+
+			// Get all products
+			ctx := context.Background()
+			products, err := s.repo.GetAll(ctx)
+			if err != nil {
+				s.logger.Error("Failed to get products for price updates", "error", err)
+				continue
+			}
+
+			// Update prices and publish events for each product
+			for _, product := range products {
+				newPrice := product.Price * rateEvent.NewRate
+
+				// Publish price update event
+				s.eventBus.Publish(events.PriceUpdate{
+					ProductID: product.ID,
+					NewPrice:  newPrice,
+					Currency:  rateEvent.Currency,
+				})
+			}
+		}
+
+	}
+}
+
+func (s *productService) GetProducts(ctx context.Context, currency string) (Products, error) {
+	s.logger.Debug("Getting all products", "currency", currency)
+
+	products, err := s.repo.GetAll(ctx)
+	if err != nil {
+		s.logger.Error("Unable to get products", "error", err)
+		return nil, err
+	}
+
+	if currency == "" {
+		return products, nil
+	}
+
+	rate, err := s.currencyService.GetRate(ctx, "EUR", currency)
+	if err != nil {
+		s.logger.Error("Unable to get currency rate", "currency", currency, "error", err)
+		return nil, err
+	}
+
+	// Create a copy of the products with updated prices
+	productCopies := make(Products, len(products))
+	for i, product := range products {
+		productCopy := *product
+		productCopy.Price = productCopy.Price * rate
+		productCopies[i] = &productCopy
+	}
+
+	return productCopies, nil
+}
+
+func (s *productService) GetProductByID(ctx context.Context, id int, currency string) (*domain.Product, error) {
+	s.logger.Debug("Getting product by ID", "id", id)
+
+	product, err := s.repo.GetById(ctx, id)
+	if err != nil {
+		s.logger.Error("Unable to get the product by ID", "id", id, "error", err)
+		return nil, err
+	}
+
+	if currency == "" {
+		return product, nil
+	}
+
+	rate, err := s.currencyService.GetRate(ctx, "EUR", currency)
+	if err != nil {
+		s.logger.Error("Unable to get currency rate", "currency", currency, "error", err)
+		return nil, err
+	}
+
+	// Create a copy of the product with updated price
+	productCopy := *product
+	productCopy.Price = productCopy.Price * rate
+	return &productCopy, nil
+}
+
+func (s *productService) UpdateProduct(ctx context.Context, product *domain.Product) error {
+	s.logger.Debug("Updating product", "id", product.ID)
+
+	err := s.repo.Update(ctx, product)
+	if err != nil {
+		s.logger.Error("Unable to update product", "id", product.ID, "error", err)
+		return err
+	}
+
+	// Publish an event for product update
+	s.eventBus.Publish(events.ProductUpdated{ProductID: product.ID})
+	return nil
+}
+
+func (s *productService) AddProduct(ctx context.Context, product *domain.Product) error {
+	s.logger.Debug("Adding new product", "name", product.Name)
+
+	err := s.repo.Add(ctx, product)
+	if err != nil {
+		s.logger.Error("Unable to add product", "name", product.Name, "error", err)
+		return err
+	}
+
+	// Publish an event for product addition
+	s.eventBus.Publish(events.ProductAdded{ProductID: product.ID})
+	return nil
+}
+
+func (s *productService) DeleteProduct(ctx context.Context, id int) error {
+	s.logger.Debug("Deleting product", "id", id)
+
+	err := s.repo.Delete(ctx, id)
+	if err != nil {
+		s.logger.Error("Unable to delete product", "id", id, "error", err)
+		return err
+	}
+
+	// Publish an event for product deletion
+	s.eventBus.Publish(events.ProductDeleted{ProductID: id})
+	return nil
+}
+
+func (s *productService) ListCurrencies(ctx context.Context) ([]string, error) {
+	s.logger.Debug("Listing available currencies")
+
+	currencies, err := s.currencyService.ListAvailableCurrencies(ctx)
+	if err != nil {
+		s.logger.Error("Unable to list available currencies", "error", err)
+		return nil, err
+	}
+
+	return currencies, err
+}
+
+func (s *productService) Close() error {
+	if s.rateSubscriber != nil {
+		s.eventBus.Unsubscribe(s.rateSubscriber)
+	}
+	return nil
+}
